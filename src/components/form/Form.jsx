d02929afefc3a60aea8acfc47b2fbab48b20f115
ohelixa <oassadi@helixa.ai>
2020-04-24 19:04:03 +0200

    Added useCallback to handle the form submit

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 export default function Form({ onSubmit, children, ...rest }) {
+  const handleFormSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      onSubmit(e);
+    },
+    [onSubmit],
+  );
   return (
-    <form
-      {...rest}
-      onSubmit={(e) => {
-        e.preventDefault();
-        onSubmit();
-      }}
-    >
+    <form {...rest} onSubmit={handleFormSubmit}>
       {children}
     </form>
   );
